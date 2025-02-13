02e539e61c256a1f4fb7fd071af8ab467353a6c0
“AntonioAmbrosecchia” <antonyambro96@gmail.com>
2025-02-13 10:35:13 +0100

    end 1.es

---
+++ main.js
@@ -0,0 +1,13 @@
+/*
+L’utente inserisce due numeri in successione, con due prompt.
+Il software stampa il maggiore.
+*/
+
+const numero1 = prompt("digita un numero");
+const numero2 = prompt("digita un numero");
+if (numero1 > numero2) {
+    console.log(numero1);
+
+} else {
+    console.log(numero2);
+}
