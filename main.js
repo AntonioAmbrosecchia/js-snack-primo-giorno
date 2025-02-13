cf228f05327501e430103d4e1491639ac6f7f47e
“AntonioAmbrosecchia” <antonyambro96@gmail.com>
2025-02-13 12:01:57 +0100

    end es.3

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 //L’utente inserisce due numeri in successione, con due prompt.
 //Il software stampa il maggiore.
 
-
+/*
 const numero1 = prompt("digita un numero");
 const numero2 = prompt("digita un numero");
 if (numero1 > numero2) {
@@ -10,17 +10,37 @@ if (numero1 > numero2) {
 } else {
     console.log(numero2);
 }
+*/
+
 
 
 //L’utente inserisce due parole in successione, con due prompt.
 //Il software stampa prima la parola più corta, poi la parola più lunga.
 
+/*
 const firstname = prompt("Digita il primo nome")
 const secondname = prompt("Digita il secondo nome")
-if (firstname.length > secondname.length) {
+if (firstname.length < secondname.length) {
     console.log(firstname, secondname);
 
-} else if (secondname.length > firstname.length) {
+} else if (secondname.length < firstname.length) {
     console.log(secondname, firstname);
-
 }
+*/
+
+
+
+//Il software deve chiedere per 10 volte all’utente di inserire un numero.
+//Il programma stampa la somma di tutti i numeri inseriti.
+const firstnumber = Number(prompt("tipe a number"));
+const secondnumber = Number(prompt("tipe a number"));
+const thirdnumber = Number(prompt("tipe a number"));
+const fourthnumber = Number(prompt("tipe a number"));
+const fifthnumber = Number(prompt("tipe a number"));
+const sixthnumber = Number(prompt("tipe a number"));
+const seventhnumber = Number(prompt("tipe a number"));
+const eighthnumber = Number(prompt("tipe a number"));
+const ninthnumber = Number(prompt("tipe a number"));
+const tenthnumber = Number(prompt("tipe a number"));
+console.log(firstnumber + secondnumber + thirdnumber + fourthnumber +
+    fifthnumber + sixthnumber + seventhnumber + eighthnumber + ninthnumber + tenthnumber);
